f1ec630c7d04f77460853b1565a45a40c4ee0cb5
wsidl <will.siddall+gitlab@gmail.com>
2024-08-09 18:05:21 -0700

    feat: add separator property for uuid generator

--- syntrend/generators/uuid.py
+++ syntrend/generators/uuid.py
@@ -8,19 +8,21 @@ class UUIDGenerator(PropertyGenerator):
     default_config = {
         "use_upper": False,
         "compact": False,
+        "separator": "-",
     }
     required_modules = ["uuid"]
 
     def load_kwargs(self, kwargs: dict[str, any]) -> dict[str, any]:
         kwargs["use_upper"] = bool(kwargs["use_upper"])
         kwargs["compact"] = bool(kwargs["compact"])
+        kwargs["separator"] = str(kwargs["separator"])
         return kwargs
 
     def generate(self):
         uuid_val = self.modules.uuid.uuid4()
         if self.kwargs.compact:
             uuid_val = uuid_val.hex
-        uuid_val = str(uuid_val)
+        uuid_val = str(uuid_val).replace("-", self.kwargs.separator)
         if self.kwargs.use_upper:
-            uuid_val = str(uuid_val).upper()
+            uuid_val = uuid_val.upper()
         return uuid_val
